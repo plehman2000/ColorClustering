e84bb6c19e57b72a608f5856d4457f3121b6fdfb
Patrick Lehman <54288924+plehman2000@users.noreply.github.com>
2022-04-18 15:43:48 -0400

    streamlit

--- streamlit/main.py
+++ streamlit/main.py
@@ -30,8 +30,8 @@ class ClusterImage:
 
     def get_clustered_image(self, clusters = 10, clustering_type = "KMeans"):
         match clustering_type:
-            case "Kmeans":
-                st.write('kmeans')
+            case "KMeans":
+                # st.write('kmeans')
                 kmeans = KMeans(n_clusters=clusters, random_state=0, max_iter = 10, n_init=1).fit(self.image_array)
                 for i, pixel in enumerate(stqdm(self.image_array)):
                     cluster = kmeans.labels_[i]
@@ -39,8 +39,13 @@ class ClusterImage:
         
         self.cluster_image_obj = Image.fromarray(np.reshape(self.clustered_image_array, self.shape))
 
-    def graph_color_space(self):
-        image_info = self.image_array
+    def graph_color_space(self, clustered_colors=False):
+        if clustered_colors:   
+            image_info = self.clustered_image_array
+            TITLE = "Reduced Image Colorspace"
+        else:
+            image_info = self.image_array
+            TITLE = "Original Image Colorspace"
         colors = np.unique(image_info, axis=0)
         color_names = []
         for col in (colors):
@@ -48,7 +53,7 @@ class ClusterImage:
         data = list(map(list, zip(colors[:,0], colors[:,1],colors[:,2],color_names)))
         new_df = pd.DataFrame(data,columns=['Red','Green','Blue', "Name"])
         colfig = px.scatter_3d(new_df, x='Red', y='Green', z='Blue',
-                    color='Blue')
+                    color='Blue', title= TITLE)
         return colfig
 image_uploaded = False
 image_file = st.file_uploader("Upload Images", type=["png","jpg","jpeg"])
@@ -65,13 +70,22 @@ if image_file is not None:
         cluster_image = ClusterImage((load_image(image_file)))
         st.write(f"Shape:{cluster_image.shape}")
 
-selection = st.selectbox("Select clustering algorithm", ["Kmeans", "GMM", "DBSCAN"], disabled= not image_uploaded)
+selection = st.selectbox("Select clustering algorithm", ["KMeans", "GMM", "DBSCAN"], disabled= not image_uploaded)
 num_clusters = st.slider("Number of Clusters", 1,50,1)
 st.write(num_clusters)
 if st.button("Begin Clustering", help="Start the color clustering algorithm", disabled= not image_uploaded):
-    st.write("Cluster...")
+    st.write("Clustering...")
     cluster_image.get_clustered_image(num_clusters,selection)
     st.write("Cluster Done")
 
-    figure = cluster_image.graph_color_space()
-    st.plotly_chart(figure, use_container_width=True)
+    figure_unclustered = cluster_image.graph_color_space(clustered_colors=False)
+    figure_clustered = cluster_image.graph_color_space(clustered_colors=True)
+    col1, col2 = st.columns(2)
+    col1.title("Original Image")
+    col1.image(cluster_image.image_obj)
+    col1.plotly_chart(figure_unclustered, use_container_width=True)
+    col2.title("Clustered Image")
+    col2.image(cluster_image.cluster_image_obj)
+    col2.plotly_chart(figure_clustered, use_container_width=True)
+    # plotly_chart(figure_clustered, use_container_width=False)
+    # st.image(cluster_image.cluster_image_obj,width=250)
